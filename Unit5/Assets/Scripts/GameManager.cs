cf4665361bf3c47618bf9ecfb9418d3b39e08a63
Emil <97280425+pajunenemil@users.noreply.github.com>
2022-03-24 12:03:04 +0200

    5.2 tehty ja 5.3 aloitettu

--- Unit5/Assets/Scripts/GameManager.cs
+++ Unit5/Assets/Scripts/GameManager.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
     private float spawnRate = 1.0f;
+    private int score;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+
+        score = 0;
+        UpdateScore(0);
+
+        gameOverText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -22,10 +31,18 @@ public class GameManager : MonoBehaviour
     IEnumerator SpawnTarget()
     {
         while (true)
-        yield return new WaitForSeconds(spawnRate);
-        int index = Random.Range(0, targets.Count);
-        Instantiate(targets[index]);
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+
+        }
+
     }
 
-
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
 }
